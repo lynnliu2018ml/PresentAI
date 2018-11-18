9328b0acb0ef7b4560dfa1fc6bda528c0a3c28f2
varunvarghese <varunmuriyanat@gmail.com>
2018-11-18 16:52:26 -0500

    Reads a file downloaded from gutenberg http://www.gutenberg.org/cache/epub/21580/pg21580.txt

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-print("test")
+import re
+
+fp = open("pg21580.txt","r")
+lines = fp.readlines()
+
+for line in lines[0:10]:
+    line = line.strip() #removes the newline at the end of the line
+    line = line.lower() #converts to lowercase
+    
+    print(line.strip())
