316a964394f7aac695a7e7ae6427752a7b893546
tblakeley7 <tyler.blakeley@outlook.com>
2018-11-24 13:54:09 -0500

    Update Project_PullBooks.py
    removed digits columns

--- Project_PullBooks.py
+++ Project_PullBooks.py
@@ -2,18 +2,34 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
+from string import digits
 from sklearn.feature_extraction.text import CountVectorizer
+
+
+wpt = nltk.WordPunctTokenizer()
+stop_words = nltk.corpus.stopwords.words('english')
+
+
+remove_digits = str.maketrans('', '', digits)
+
+
 def normalize_document(doc):
     # lower case and remove special characters\whitespaces
     doc = re.sub(r'[^a-zA-Z0-9\s]', '', doc, re.I)
+
     doc = doc.lower()
     doc = doc.strip()
+    doc = doc.translate(remove_digits)
     # tokenize document
     tokens = wpt.tokenize(doc)
+
     # filter stopwords out of document
     filtered_tokens = [token for token in tokens if token not in stop_words]
+
+    #filtered_tokens = [token for token in tokens if token == "000"]
     # re-create document from filtered tokens
     doc = ' '.join(filtered_tokens)
+    print("run")
     return doc
 
 
@@ -24,17 +40,16 @@ from gutenberg.cleanup import strip_headers
 
 
 text = strip_headers(load_etext(2701)).strip()
-text2 = strip_headers(load_etext(3702)).strip()
+text2 = strip_headers(load_etext(3701)).strip()
 labels = ['Moby Dick','Book2']
 corpus = np.array([text,text2])
 corpus_df = pd.DataFrame({'Document': corpus,
                           'Category': labels})
-corpus_df.head()
+print(corpus_df.head())
 
 #Tokenize words and clean them
 
-wpt = nltk.WordPunctTokenizer()
-stop_words = nltk.corpus.stopwords.words('english')
+
 
 
 normalize_corpus = np.vectorize(normalize_document)
