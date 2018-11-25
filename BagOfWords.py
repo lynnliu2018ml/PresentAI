3e827e17c01a494b37a058be44d90786de7460f6
varunvarghese <varunmuriyanat@gmail.com>
2018-11-25 01:52:40 -0500

    Added Lemmitization. Wordcount came down from 25K to 20K

--- BagOfWords.py
+++ BagOfWords.py
@@ -5,6 +5,14 @@ import nltk
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 
+
+# Wordnet is an large, freely and publicly available lexical database for the English language 
+# aiming to establish structured semantic relationships between words. 
+# It offers lemmatization capabilities as well and is one of the earliest and most commonly used lemmatizers.
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+
+
 wpt = nltk.WordPunctTokenizer()
 stop_words = nltk.corpus.stopwords.words('english')
 
@@ -21,10 +29,23 @@ def normalize_document(doc):
     doc = ' '.join(filtered_tokens)
     return doc
 
+def Lemmitize(s):
+    # Init the Wordnet Lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_output = " ".join([lemmatizer.lemmatize(w) for w in s.split(" ")])
+    return lemmatized_output
+    
+def RemoveNumbers(s):
+    s = re.sub(r'\d+',' ', s)
+    return s
+
+def RemoveSpecialCharacters(s):
+    return s.replace('_', ' ')
 
 def RemoveUnicodeChars(s):
     #remove the ZERO WIDTH NO-BREAK SPACE unicode character
     s = s.replace(u'\ufeff', '')
+    s = re.sub(r'[^\x00-\x7F]+',' ', s) #Replace non-ASCII characters with a single space
     return s
 
 def GetBooks():
@@ -37,8 +58,13 @@ def GetBooks():
         with open(folderPath + file, encoding="utf8") as f:
             lines = f.readlines()
             long_string = " ".join(lines)
-            long_string = RemoveUnicodeChars(long_string)
             long_string = normalize_document(long_string)
+            long_string = RemoveUnicodeChars(long_string)
+            long_string = RemoveNumbers(long_string)
+            long_string = RemoveSpecialCharacters(long_string)
+#             print(long_string)
+            long_string = Lemmitize(long_string) 
+            long_string = " ".join(list(set(long_string.split(" ")))) #retain only the unique words
             book_name = file.split(".")[0] #extract the filename without the extension
             labels.append(book_name)
             corpus.append(long_string)
@@ -51,25 +77,14 @@ corpus_df = GetBooks()
 print(GetBooks().head(10))
 
 
-# corpus = np.array(corpus)
-# corpus_df = pd.DataFrame({'Document': corpus, 'Category': labels})
-# corpus_df = corpus_df[['Document', 'Category']]
-# corpus_df
-
-
-
-# normalize_corpus = np.vectorize(normalize_document)
-#   
-# norm_corpus = normalize_corpus(corpus)
-# norm_corpus
-#  
  
-# #Bag of Words Model
-# cv = CountVectorizer(min_df=0., max_df=1.)
-# cv_matrix = cv.fit_transform(norm_corpus)
-# cv_matrix = cv_matrix.toarray()
-# cv_matrix
-# 
-# vocab = cv.get_feature_names()
-# print(pd.DataFrame(cv_matrix, columns=vocab))
-
+#Bag of Words Model
+cv = CountVectorizer(min_df=0., max_df=1.)
+cv_matrix = cv.fit_transform(corpus_df["Document"])
+cv_matrix = cv_matrix.toarray()
+cv_matrix
+ 
+vocab = cv.get_feature_names()
+print(pd.DataFrame(cv_matrix, columns=vocab))
+print(len(vocab))
+#print(vocab)
