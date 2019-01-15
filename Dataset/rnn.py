d7ff62e6feac1fad0babb1ae7e366c86ebf34ab5
Andy-SZ <45152816+Andy-SZ@users.noreply.github.com>
2019-01-15 03:18:23 -0500

    Add files via upload
    RNN Model

---
+++ Dataset/rnn.py
@@ -0,0 +1,44 @@
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from sklearn.model_selection import train_test_split
+
+
+data.columns=['text','sentiment']
+data=data.astype(str)
+
+
+max_fatures = 2000
+tokenizer = Tokenizer(num_words=max_fatures, split=' ')
+tokenizer.fit_on_texts(data['text'].values)
+X = tokenizer.texts_to_sequences(data['text'].values)
+X = pad_sequences(X)
+
+embed_dim = 128
+lstm_out = 196
+
+model = Sequential()
+model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+model.add(SpatialDropout1D(0.4))
+model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(2,activation='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+print(model.summary())
+
+Y = pd.get_dummies(data['sentiment']).values
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.33, random_state = 42)
+print(X_train.shape,Y_train.shape)
+print(X_test.shape,Y_test.shape)
+
+
+batch_size = 32
+model.fit(X_train, Y_train, epochs = 3, batch_size=batch_size, verbose = 2)
+
+
+score,acc = model.evaluate(X_test, Y_test, verbose = 2)
+print("score: %.2f" % (score))
+print("acc: %.2f" % (acc))
+
